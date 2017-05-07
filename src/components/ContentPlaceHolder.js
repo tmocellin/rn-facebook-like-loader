978d4560ed057b7d54020ca55bc08903f9eb35e8
Tybi <developer@dwastudio.fr>
2017-05-07 16:20:58 +0200

    add AnimatedView & LinearGradient

--- src/components/ContentPlaceHolder.js
+++ src/components/ContentPlaceHolder.js
@@ -3,14 +3,20 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Animated,
+  Easing
 } from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
+
+const gradientWidth = 75;
 
 export default class ContentPlaceHolder extends Component {
 
   constructor () {
     super();
+    this.animatedValue = new Animated.Value(0);
     this.state = {
       width:0
     }
@@ -23,11 +29,38 @@ export default class ContentPlaceHolder extends Component {
     });
   }
 
+  animate () {
+    this.animatedValue.setValue(0)
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: 1,
+        duration: 800,
+        easing: Easing.linear
+      }
+    ).start(() => this.animate())
+  }
+
+  componentDidMount () {
+    this.animate()
+  }
+
   render() {
+
+    const marginEnd = this.state.width - gradientWidth;
+    const marginLeft = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, marginEnd]
+    });
+
     return (
       <View style={styles.wrapper}>
         <View style={styles.background} onLayout={(event) => { this._onLayout(event) }}>
-
+          <Animated.View style={[styles.linGradient,{marginLeft}]}>
+            <LinearGradient
+              start={{x: 0, y: 0}} end={{x: 1, y: 0}}
+              colors={['#f6f7f8', '#e8e8e8', '#dddddd']}  style={styles.linGradient} />
+          </Animated.View>
         </View>
       </View>
     );
@@ -47,5 +80,13 @@ const styles = StyleSheet.create({
   },
   background:{
     backgroundColor:'#f6f7f8',
+    flex:1
+  },
+  linGradient:{
+    width:gradientWidth,
+    position:'absolute',
+    top:0,
+    left:0,
+    bottom:0,
   },
 });
